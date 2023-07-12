03b608caca4337cac80286562d2b7d4cbfff7ed3
fabio.ochoa <fabio8dev@gmail.com>
2023-07-11 23:08:52 -0500

    vs-202-github-collaboration-20230711-001

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vs_2022_github_collaboration.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
